deae26017ba1680d6a2eeb207716a5f4350e509c
Laetitia DECRE <laetitia.decre@gmail.com>
2022-07-27 13:46:42 +0200

    create static page

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,17 +2,29 @@
 
 namespace App\Controller;
 
+use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form ->isValid()) {
+            $this->addFlash('notice', "Merci de m'avoir contacter. Je vous répond dans les meilleurs délais.");
+
+           /* $mail = new Mail();
+            $mail->send('contact@sandrine-coupart.fr', 'Sandrine Coupart', 'Vous avez reçu une demande de contact');*/
+        }
+
         return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
+            'form' =>$form->createView()
         ]);
     }
 }
