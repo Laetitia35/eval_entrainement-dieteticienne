38d44c6792cf38233585adfc7e4e571f94c44bb3
Laetitia DECRE <laetitia.decre@gmail.com>
2022-08-19 17:48:38 +0200

    Register form

--- src/Controller/RegisterAdminController.php
+++ src/Controller/RegisterAdminController.php
@@ -3,17 +3,38 @@
 namespace App\Controller;
 use App\Entity\User;
 use App\Form\RegisterAdminType;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class RegisterAdminController extends AbstractController
 {
+     
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager) {
+        $this->entityManager = $entityManager;
+    }
+
     #[Route('/inscription', name: 'app_register_admin')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
         $user = new User();
         $form = $this->createForm(RegisterAdminType::class, $user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+
+            $user = $form->getData();
+
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
+        }
 
         return $this->render('register_admin/index.html.twig', [
             'form' => $form->createView(),
--- src/Form/RegisterAdminType.php
+++ src/Form/RegisterAdminType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ButtonType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -19,36 +20,50 @@ class RegisterAdminType extends AbstractType
         $builder
 
             ->add('firstname', TextType::class, [
-                'label' =>'Prenom',
+                'label' =>'Prenom :',
                 'attr' =>[
                     'placeholder' => 'Merci de saisir le prenom du patient'
                 ]
             ])
 
             ->add ('lastname',  TextType::class, [
-                'label' =>'Nom',
+                'label' =>'Nom :',
                 'attr' =>[
                     'placeholder' => 'Merci de saisir le nom du patient'
                 ]
             ])
 
             ->add('email',   EmailType::class, [
-                'label' =>'Email',
+                'label' =>'Email :',
                 'attr' =>[
                     'placeholder' => "Merci de saisir l'adresse email du patient"
                 ]
             ])
             
-            ->add('password', PasswordType::class, [
-                'label' => ' Insérer le mot de passe générer',
-                'attr' => [
-                    'placeholder' => 'Merci de saisir le mot de passe générer'
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class, 
+                'invalid_message' =>'Le mot de passe et la confirmation doivent être identique',
+                'label' => 'Insérer le mot de passe générer',
+                'required' => true,
+                'first_options' => [
+                    'label' =>'Mot de passe générer :',
+                    'attr' => [
+                        'placeholder' => 'Merci de saisir le mot de passe sécurisé générer aléatoirement.'
+                    ]
+                ],
+                'second_options' => [
+                    'label' => 'Confirmer le mot de passe générer aléatoirement :',
+                    'attr' => [
+                        'placeholder' => 'Merci de confirmer la saisie du mot de passe sécurisé générer aléatoirement.'
+                    ]
+                    
                 ]
+                
             ])
 
-            ->add('passwordGenerate', ButtonType::class, [
-                'label' =>'genérer mot de passe'
-            ]) 
+            //->add('passwordGenerate', ButtonType::class, [
+                //'label' =>'genérer le mot de passe aléatoire'
+            //]) 
 
             ->add('submit', SubmitType::class, [
                 'label' => "Valider l'inscription"
